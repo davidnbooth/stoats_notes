2f8b08877898bc28ea479d587a0ce37459998a00
David Booth <davidnbooth@gmail.com>
2023-07-12 08:45:31 -0500

    debugging DBConnection

--- lib/DBConnection.ts
+++ lib/DBConnection.ts
@@ -1,4 +1,4 @@
-import { createPool, PoolConnection } from "mariadb";
+import { createPool, PoolConnection, PoolConfig } from "mariadb";
 import fs from "fs";
 import os from "os";
 
@@ -15,12 +15,15 @@ const DBConnection = {
     makeNewConnectionWithTable: async (mode?: "development"|"production", secrets?: {sql_user: string, sql_password: string}, tableName?: string): Promise<PoolConnection> => {
         const dbHost = dbHosts[mode || defaultMode];
 
-        const pool = await createPool({
+        const connectionOptions: PoolConfig = {
             host: dbHost,
             user: secrets ? secrets.sql_user : defaultSecrets.sql_user,
             password: secrets ? secrets.sql_password : defaultSecrets.sql_password,
             database: "stoats"
-        });
+        };
+
+        console.log(JSON.stringify(connectionOptions));
+        const pool = await createPool(connectionOptions);
         const connection = await pool.getConnection();
         await connection.query(`CREATE TABLE IF NOT EXISTS ${tableName || defaultTableName} (NoteID INT PRIMARY KEY, Content TEXT)`);
         return connection;

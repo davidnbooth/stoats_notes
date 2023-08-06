b7ab4cd901f07a053087551df0874a2283e3510e
David Booth <davidnbooth@gmail.com>
2023-08-06 10:23:56 -0400

    another visible change to test deployment

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default async function Home({}) {
 
     return (
         <main>
-            <h2>Welcome to Stoats Notes!</h2>
+            <h2>Welcome to Stoats Notes!!</h2>
             <form>
                 <textarea id={styles["note"]} name="note" defaultValue={usNoteContent}>
                 </textarea>
